819a9c15f5c6bfd4fe991b9bdca7083b9c79e304
calebeno <caleb.eno@gmail.com>
2018-10-16 09:44:24 -0400

    Create lunch-2000.js

---
+++ lunch-2000.js
@@ -0,0 +1,46 @@
+var express    = require('express');
+var Webtask    = require('webtask-tools');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.sendStatus(200);
+});
+
+app.get('/food', (req, res) => {
+  req.webtaskContext.storage.get((err, data) => {
+    if (err) res.sendStatus(500);
+    res.json(data.foodPlaces);
+  });
+});
+
+app.get('/attendees', (req, res) => {
+  req.webtaskContext.storage.get((err, data) => {
+    if (err) res.sendStatus(500);
+    res.json(data.lunchAttendees);
+  });
+});
+
+app.post('/addFood', (req, res) => {
+  console.log(req.body);
+  req.webtaskContext.storage.get((err, data) => {
+    if (err) res.sendStatus(500);
+    let food = data.foodPlaces;
+
+    if (typeof req.body.name === 'string') {
+      food.push({
+        name: req.body.name,
+        okOption: true
+      });
+    }
+    data.foodPlaces = food;
+    req.webtaskContext.storage.set(data, (error) => {
+      if (error) res.sendStatus(500);
+      res.sendStatus(200);
+    });
+  })
+});
+
+module.exports = Webtask.fromExpress(app);
