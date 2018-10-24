b1f17279e9e6a87176054059b77a1a9f7d60f123
Caleb Eno <caleb.eno@gmail.com>
2018-10-24 13:57:51 -0400

    Committing Dist Folder For Server

---
+++ lunch-picker-server/dist/src/lunch-2000.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let express = require('express'); // tslint:disable-line
+let bodyParser = require('body-parser'); // tslint:disable-line
+let Webtask = require('webtask-tools'); // tslint:disable-line
+const app = express();
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+app.get('/food', (req, res) => {
+    req.webtaskContext.storage.get((err, data) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.json(data.foodPlaces);
+    });
+});
+app.get('/attendees', (req, res) => {
+    req.webtaskContext.storage.get((err, data) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        res.json(data.lunchAttendees);
+    });
+});
+app.post('/addFood', (req, res) => {
+    console.log(req.body);
+    req.webtaskContext.storage.get((err, data) => {
+        if (err) {
+            res.sendStatus(500);
+        }
+        const food = data.foodPlaces;
+        if (typeof req.body.name === 'string') {
+            food.push({
+                name: req.body.name,
+                okOption: true,
+            });
+        }
+        data.foodPlaces = food;
+        req.webtaskContext.storage.set(data, (error) => {
+            if (error) {
+                res.sendStatus(500);
+            }
+            res.sendStatus(200);
+        });
+    });
+});
+module.exports = Webtask.fromExpress(app);
